288ecdb216737ce09ac8196efeb8dd6030410881
heymorsa <66024230+heymorsa@users.noreply.github.com>
2020-06-10 15:41:08 +0300

    Delete Shield.java

--- ООП-6/Shield.java
+++
@@ -1,9 +0,0 @@
-public class Shield extends Ammunition {
-    Shield(String n, int w, int c, int d, int p){
-        this.setName(n);
-        this.setWeight(w);
-        this.setCost(c);
-        this.setDamage(d);
-        this.setProtection(p);
-    }
-}
